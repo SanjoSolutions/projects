5a73503d046df3eb131ec9925711a3497000a3b0
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2020-03-21 20:44:53 +0100

    Fixes submitting form of gewobag by click on submit button instead of submitting form with form.submit()
    Fixes error that object has not been found, that appeared after submitting the filled form.

--- src/modules/gewobag/applyForFlatOffer.js
+++ src/modules/gewobag/applyForFlatOffer.js
@@ -1,17 +1,16 @@
 import { closePage } from '../../lib/closePage.js'
 import { createApplyForFlatOffer } from '../../lib/createApplyForFlatOffer.js'
-import { createSubmitFormAndVerifySuccess } from '../../lib/createSubmitFormAndVerifySuccess.js'
 import { saveScreenshotOfFlatOfferApplication } from '../../lib/saveScreenshotOfFlatOfferApplication.js'
 import { fillForm } from './fillForm.js'
 import { openForm } from './openForm.js'
-import { verifyFormSubmissionSuccess } from './verifyFormSubmissionSuccess.js'
+import { submitForm } from './submitForm.js'
 import { verifyRequiredFields } from './verifyRequiredFields.js'
 
 export const applyForFlatOffer = createApplyForFlatOffer({
   verifyRequiredFields,
   openForm,
   fillForm,
-  submitForm: createSubmitFormAndVerifySuccess(verifyFormSubmissionSuccess),
+  submitForm,
   saveScreenshot: saveScreenshotOfFlatOfferApplication,
   closePage
 })
