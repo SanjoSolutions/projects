9228d9a4289dd88f036891a453bc33eeb9460bb9
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2020-08-22 21:32:39 +0200

    Space with combination of Map and Sets

---
+++ Space2.js
@@ -0,0 +1,18 @@
+export class Space {
+  constructor(viewport) {
+    this.data = new Map()
+  }
+
+  get({x, y}) {
+    return Boolean(this.data.get(y)?.has(x))
+  }
+
+  set({x, y}) {
+    let lookup = this.data.get(y)
+    if (!lookup) {
+      lookup = new Set()
+      this.data.set(y, lookup)
+    }
+    lookup.add(x)
+  }
+}
