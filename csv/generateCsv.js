70634400bd20ddcd302b7b1cf0fc0bc195293d79
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2020-09-02 23:47:43 +0200

    Renames csvToText to generateCsv

--- csv/generateCsv.js
+++ csv/generateCsv.js
@@ -3,9 +3,9 @@
  * Comma is used as separator for the cells.
  * Each value is wrapped in quotation marks (").
  * New line is used as line separator.
- * @param csv {any[][]} Array of rows. Each row is an array with all the cells.
+ * @param data {any[][]} Array of rows. Each row is an array with all the cells.
  * @returns {string} CSV
  */
-export function csvToText (csv) {
-  return csv.map(row => row.map(cell => `"${cell}"`).join(',')).join('\n')
+export function generateCsv(data) {
+  return data.map(row => row.map(cell => `"${cell}"`).join(',')).join('\n')
 }
