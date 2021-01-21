9e404731bdc4b51eb87783a96d057f9c2e57b5b4
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-01-21 18:32:34 +0100

    Revalidation of hypothesis for calibrated Consciousness Level for
    the Great Books of the Western World in Power vs. Force being a
    average value of the individual calibrated Consciousness Levels of the Books of the Western World. Hypothesis validated as untrue.
    Also validated hypothesis that the calibrated Consciousness Level of the Great Books of the Western World equals the median value of the individual Great Books of the Western World. Hypothesis validated as untrue.
    
    Also verified that the used functions work.

--- experiment_calibration_level_of_collection_of_calibrations/experiment_calibration_level_of_collection_of_calibrations.js
+++ experiment_calibration_level_of_collection_of_calibrations/experiment_calibration_level_of_collection_of_calibrations.js
@@ -72,20 +72,32 @@ const calibrations = [
   445,
 ]
 
-const calibrationsValues = calibrations.map(calibration => BigInt(10) ** BigInt(calibration))
+const calibrationsValues = calibrations.map(calibration => BigInt(10) **
+  BigInt(calibration))
 const averageCalibrationValue = approximatedLog10(average(calibrationsValues))
-console.log(averageCalibrationValue)
+console.log('average:', averageCalibrationValue)
+console.log('median:', approximatedLog10(median(calibrationsValues)))
 
-function average(values) {
+function average (values) {
   return values.reduce((sum, value) => sum + value) / BigInt(values.length)
 }
 
-function wholeNumberLog10(value) {
+function median (values) {
+  values.sort()
+  if (values.length % 2 == 0) {
+    return (values[values.length / 2 - 1] + values[values.length / 2]) / 2n
+  } else {
+    return values[Math.floor(values.length / 2)]
+  }
+}
+
+function wholeNumberLog10 (value) {
   const valueString = value.toString()
   return valueString.length
 }
 
-function approximatedLog10(value) {
+export function approximatedLog10 (value) {
   const valueString = value.toString()
   return valueString.length + Math.log10(Number(`0.${valueString}`))
 }
+
---
+++ experiment_calibration_level_of_collection_of_calibrations/experiment_calibration_level_of_collection_of_calibrations.tests.js
@@ -0,0 +1,4 @@
+import { approximatedLog10 } from './experiment_calibration_level_of_collection_of_calibrations.js'
+
+console.log(approximatedLog10(BigInt(2784712984712948712)))
+console.log('18.444780439876409072366528664389760706970726257214358104947905203')
