e3bd3781536533a11d748202922b992098e3d90f
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-09-15 17:27:05 +0200

    Adds function useDocumentSnapshot

---
+++ react/firebase/useDocumentSnapshots.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+import { identity } from '../../packages/identity/src/identity.js'
+
+export function useDocumentSnapshot(
+  queryRef,
+  filterFn = identity,
+) {
+  const [documents, setDocuments] = useState(null)
+
+  useEffect(
+    () => {
+      setDocuments(null)
+      let unsubscribe
+
+      async function retrieve() {
+        unsubscribe = queryRef.onSnapshot(
+          snapshot => {
+            setDocuments(filterFn(snapshot.docs))
+          },
+        )
+      }
+
+      retrieve()
+
+      return () => {
+        if (unsubscribe) {
+          unsubscribe()
+        }
+      }
+    },
+    [
+      queryRef,
+      filterFn,
+    ],
+  )
+
+  return documents
+}
