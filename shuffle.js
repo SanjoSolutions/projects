8ede6df2c04816f5aa1058026f374a4c839eafda
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-09-15 22:20:20 +0200

    Adds function shuffle

---
+++ shuffle.js
@@ -0,0 +1,8 @@
+export function shuffle(values) {
+  const shuffledValues = []
+  for (const value of values) {
+    const index = randomInteger(0, shuffledValues.length)
+    shuffledValues.splice(index, 0, value)
+  }
+  return shuffledValues
+}
