85ad161b449aaa0aff99cac226b1e62bdc59b8aa
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-09-19 22:26:14 +0200

    Moves JavaScript code into a JavaScript file
    And extracts out function delayed.
    And extends function delayed, to be cancellable.

---
+++ delayed.js
@@ -0,0 +1,21 @@
+export function delayed(fn, delay) {
+  let handler = null;
+
+  function delayedFn(...args) {
+    if (!handler) {
+      handler = setTimeout(() => {
+        handler = null
+        fn(...args)
+      }, delay)
+    }
+  }
+
+  delayedFn.cancel = function () {
+    if (handler) {
+      clearTimeout(handler)
+      handler = null
+    }
+  }
+
+  return delayedFn
+}
