5d20eef71d9a8a9f5412acefffdbefed9a8caf23
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-01-01 23:40:33 +0100

    random: fixes type declaration

--- packages/random/src/getRandomValue.ts
+++ packages/random/src/getRandomValue.ts
@@ -1,5 +1,5 @@
 import { getRandomInteger } from './getRandomInteger.js'
 
-export function getRandomValue(values: []) {
+export function getRandomValue(values: any[]) {
   return values[getRandomInteger(0, values.length)]
 }
