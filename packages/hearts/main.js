877b07f0f967de0209a3fb03717e9d8daf9f7b85
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-01-22 18:44:39 +0100

    Hearts

--- packages/hearts/main.js
+++ packages/hearts/main.js
@@ -1,15 +1,67 @@
+import { generateRandomInteger } from '@sanjo/random';
+class Heart {
+    x;
+    y;
+    a = 0;
+    b = 0;
+    top = 0;
+    offsetY = 0;
+    // Polynomial function of 2nd degree to model the movement curve
+    xs = 0;
+    ys = 0;
+    m = 0;
+    element;
+    constructor({ x, y }) {
+        this.x = x;
+        this.y = y;
+        this.element = document.createElement('div');
+        this.element.textContent = '❤';
+        this.element.classList.add('heart');
+        document.body.appendChild(this.element);
+    }
+    generateNewAAndB() {
+        this.a = generateRandomInteger(128, 182 + 1);
+        this.b = generateRandomInteger(-200, 200 + 1);
+        this.offsetY = 0;
+        this.xs = 0;
+        this.ys = this.b;
+        /**
+         * @source https://www.wolframalpha.com/input/?i2d=true&i=0%3Dm*Power%5B%5C%2840%29a-e%5C%2841%29%2C2%5D%2Bd
+         * d = ys
+         * e = xs
+         */
+        this.m = -(this.ys / ((this.xs - this.a) ** 2));
+    }
+    f(x) {
+        return this.m * (x - this.xs) ** 2 + this.ys;
+    }
+}
 export function main() {
-    let top = 0;
-    const heart = document.createElement('div');
-    heart.textContent = '❤';
-    heart.classList.add('heart');
-    document.body.appendChild(heart);
+    const hearts = [];
+    function spawnHearts() {
+        for (let i = 0; i < 5; i++) {
+            const heart = new Heart({ x: generateRandomInteger(0, window.innerWidth), y: -10 });
+            hearts.push(heart);
+        }
+    }
+    spawnHearts();
+    setInterval(spawnHearts, 3000);
+    hearts.forEach(heart => heart.generateNewAAndB());
     function requestNextAnimationFrame() {
         requestAnimationFrame(animate);
     }
     function animate() {
-        top += 0.125;
-        heart.style.top = `${top}px`;
+        for (const heart of hearts) {
+            const offsetX = heart.f(heart.offsetY - heart.a);
+            if (heart.offsetY >= 2 * heart.a) {
+                heart.generateNewAAndB();
+            }
+            const asd = 0.125;
+            heart.top += asd;
+            heart.offsetY += asd;
+            heart.element.style.left = `${heart.x + offsetX}px`;
+            heart.element.style.top = `${heart.top}px`;
+        }
         requestNextAnimationFrame();
     }
     requestNextAnimationFrame();
--- packages/hearts/src/main.ts
+++ packages/hearts/src/main.ts
@@ -1,17 +1,81 @@
+import { generateRandomInteger } from '@sanjo/random'
+
+class Heart {
+  public x: number
+  public y: number
+  public a: number = 0
+  public b: number = 0
+  public top: number = 0
+  public offsetY: number = 0
+
+  // Polynomial function of 2nd degree to model the movement curve
+  public xs: number = 0
+  public ys: number = 0
+  public m: number = 0
+
+  public element: HTMLElement
+
+  constructor({ x, y }: { x: number; y: number }) {
+    this.x = x
+    this.y = y
+    this.element = document.createElement('div')
+    this.element.textContent = '❤'
+    this.element.classList.add('heart')
+    document.body.appendChild(this.element)
+  }
+
+  generateNewAAndB(): void {
+    this.a = generateRandomInteger(128, 182 + 1)
+    this.b = generateRandomInteger(-200, 200 + 1)
+    this.offsetY = 0
+
+    this.xs = 0
+    this.ys = this.b
+    /**
+     * @source https://www.wolframalpha.com/input/?i2d=true&i=0%3Dm*Power%5B%5C%2840%29a-e%5C%2841%29%2C2%5D%2Bd
+     * d = ys
+     * e = xs
+     */
+    this.m = -(this.ys / (this.xs - this.a) ** 2)
+  }
+
+  f(x: number): number {
+    return this.m * (x - this.xs) ** 2 + this.ys
+  }
+}
+
 export function main(): void {
-  let top = 0
-  const heart = document.createElement('div')
-  heart.textContent = '❤'
-  heart.classList.add('heart')
-  document.body.appendChild(heart)
+  const hearts: Heart[] = []
+
+  function spawnHearts() {
+    for (let i = 0; i < 5; i++) {
+      const heart = new Heart({ x: generateRandomInteger(0, window.innerWidth), y: -10 })
+      hearts.push(heart)
+    }
+  }
+
+  spawnHearts()
+
+  setInterval(spawnHearts, 3000)
+
+  hearts.forEach(heart => heart.generateNewAAndB())
 
   function requestNextAnimationFrame(): void {
     requestAnimationFrame(animate)
   }
 
   function animate(): void {
-    top += 0.125
-    heart.style.top = `${top}px`
+    for (const heart of hearts) {
+      const offsetX = heart.f(heart.offsetY - heart.a)
+      if (heart.offsetY >= 2 * heart.a) {
+        heart.generateNewAAndB()
+      }
+      const asd = 0.125
+      heart.top += asd
+      heart.offsetY += asd
+      heart.element.style.left = `${heart.x + offsetX}px`
+      heart.element.style.top = `${heart.top}px`
+    }
     requestNextAnimationFrame()
   }
 
