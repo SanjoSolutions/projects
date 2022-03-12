f32ab01ea7028ed56efaeb8e648df07220bbc3da
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-03-12 21:52:16 +0100

    Removes unnecessary comparison of the first element with itself

--- max.js
+++ max.js
@@ -1,6 +1,6 @@
 export function max(values) {
   let maximum = values[0]
-  for (let index = 0; index <= values.length; index++) {
+  for (let index = 1; index <= values.length; index++) {
     const value = values[index]
     if (value > maximum) {
       maximum = value
