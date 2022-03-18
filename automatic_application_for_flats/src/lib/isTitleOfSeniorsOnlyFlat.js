0347f5db1b32d3a94d4b99d8905c37af0fa870ae
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-03-18 17:11:25 +0100

    Adds phrases for detecting senior only flats

--- automatic_application_for_flats/src/lib/isTitleOfSeniorsOnlyFlat.js
+++ automatic_application_for_flats/src/lib/isTitleOfSeniorsOnlyFlat.js
@@ -1,3 +1,9 @@
 export function isTitleOfSeniorsOnlyFlat(title) {
-  return title.includes('Senioren') || title.includes('ältere Generation') || title.includes('Senior')
+  return (
+    title.includes('Senioren') ||
+    title.includes('ältere Generation') ||
+    title.includes('Senior') ||
+    title.includes('55+') ||
+    title.includes('ab 60 Jahren')
+  )
 }
