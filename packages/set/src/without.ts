00f0f2204c011db82bfc75f3abef22f0e8ff329c
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-04-16 17:12:36 +0200

    without: changes function signature to have only one rest parameter

--- packages/set/src/without.ts
+++ packages/set/src/without.ts
@@ -1,6 +1,6 @@
-export function without<T>(set: Iterable<T>, ...otherSets: Iterable<T>[]): Set<T> {
-  const result = new Set(set)
-  for (const otherSet of otherSets) {
+export function without<T>(...sets: Iterable<T>[]): Set<T> {
+  const result = new Set(sets[0])
+  for (const otherSet of sets.slice(1)) {
     for (const element of otherSet) {
       result.delete(element)
     }
