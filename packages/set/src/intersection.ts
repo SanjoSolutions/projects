0dd72012c7b8dafbc5f3be3de52a8579d9f35914
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-04-16 17:08:15 +0200

    intersection: changes function signature to have only one rest parameter

--- packages/set/src/intersection.ts
+++ packages/set/src/intersection.ts
@@ -1,6 +1,6 @@
-export function intersection<T>(firstSet: Iterable<T>, ...otherSets: Iterable<T>[]): Set<T> {
-  let result = Array.from(firstSet)
-  for (const set of otherSets) {
+export function intersection<T>(...sets: Iterable<T>[]): Set<T> {
+  let result = Array.from(sets[0])
+  for (const set of sets.slice(1)) {
     result = result.filter(value => new Set(set).has(value))
   }
   return new Set(result)
