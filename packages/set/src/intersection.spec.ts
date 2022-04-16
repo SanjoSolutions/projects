5da1ef7774fbf552b3465efd7d9a496f6e731a98
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-04-16 16:50:15 +0200

    intersection: support for more than 2 sets as input

--- packages/set/src/intersection.spec.ts
+++ packages/set/src/intersection.spec.ts
@@ -5,4 +5,8 @@ describe('intersection', () => {
   it('returns the intersection of two sets', () => {
     expect(intersection(new Set([1, 2]), new Set([2, 3]))).toEqual(new Set([2]))
   })
+
+  it('supports more than 2 sets as input', () => {
+    expect(intersection(new Set([1, 2]), new Set([2, 3]), new Set([4]))).toEqual(new Set([]))
+  })
 })
--- packages/set/src/intersection.ts
+++ packages/set/src/intersection.ts
@@ -1,3 +1,7 @@
-export function intersection<T>(setA: Iterable<T>, setB: Set<T>): Set<T> {
-  return new Set([...setA].filter(value => setB.has(value)))
+export function intersection<T>(firstSet: Iterable<T>, ...otherSets: Set<T>[]): Set<T> {
+  let result = Array.from(firstSet)
+  for (const set of otherSets) {
+    result = result.filter(value => set.has(value))
+  }
+  return new Set(result)
 }
