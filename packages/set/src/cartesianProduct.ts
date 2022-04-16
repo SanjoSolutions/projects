ae9470b25735036eed81cc92d52ef7d1e2c0cba5
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-04-16 17:06:07 +0200

    cartesianProduct: allow all arguments to be iterables

--- packages/set/src/cartesianProduct.ts
+++ packages/set/src/cartesianProduct.ts
@@ -1,8 +1,12 @@
 /**
  * @see https://en.wikipedia.org/wiki/Cartesian_product
  */
-export function cartesianProduct<T, U>(setA: Set<T> | T[], setB: Set<U> | U[]): Set<[T, U]> {
+export function cartesianProduct<T, U>(setA: Iterable<T>, setB: Iterable<U>): Set<[T, U]> {
   const result = new Set<[T, U]>()
-  setA.forEach(valueA => setB.forEach(valueB => result.add([valueA, valueB])))
+  for (const elementA of setA) {
+    for (const elementB of setB) {
+      result.add([elementA, elementB])
+    }
+  }
   return result
 }
