7fd89b98e319294bd094b9b304130df32a69a48a
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-04-16 16:54:56 +0200

    difference: allow all arguments to be iterables

--- packages/set/src/difference.ts
+++ packages/set/src/difference.ts
@@ -1,4 +1,4 @@
-export function difference<T>(firstSet: Iterable<T>, ...otherSets: Set<T>[]): Set<T> {
+export function difference<T>(firstSet: Iterable<T>, ...otherSets: Iterable<T>[]): Set<T> {
   const differenceSet = new Set(firstSet)
   for (const set of otherSets) {
     for (const element of set) {
