0b2ca09df0f02f6b89243a172e9067813af01cb0
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-04-16 16:45:56 +0200

    union: support for more than 2 sets as input

--- packages/set/src/union.spec.ts
+++ packages/set/src/union.spec.ts
@@ -7,3 +7,11 @@ test('union', () => {
   const c = union(a, b)
   expect(c).toEqual(new Set([1, 2]))
 })
+
+test('union more than 2 sets', () => {
+  const a = new Set([1])
+  const b = new Set([2])
+  const c = new Set([3])
+  const result = union(a, b, c)
+  expect(result).toEqual(new Set([1, 2, 3]))
+})
--- packages/set/src/union.ts
+++ packages/set/src/union.ts
@@ -1,10 +1,10 @@
-export function union<T>(setA: Iterable<T>, setB: Iterable<T>): Set<T> {
+export function union<T>(...sets: Iterable<T>[]): Set<T> {
   const unionSet = new Set<T>()
-  for (const value of setA) {
-    unionSet.add(value)
+  for (const set of sets) {
+    for (const value of set) {
+      unionSet.add(value)
+    }
   }
-  for (const value of setB) {
-    unionSet.add(value)
-  }
+
   return unionSet
 }
