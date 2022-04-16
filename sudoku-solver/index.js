799aa9c36a0bdff84f39298311797d7995799162
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-04-16 19:04:51 +0200

    sudoku-solver: build

--- sudoku-solver/index.js
+++ sudoku-solver/index.js
@@ -1,17 +1,17 @@
 function l(...l) {
   return [].concat(...l)
 }
-function* n(l, n, u = 1) {
-  for (let e = l; e <= n; e += u) yield e
+function* n(l, n, e = 1) {
+  for (let u = l; u <= n; u += e) yield u
 }
-function u(l) {
+function e(l) {
   let n = [[]]
-  for (const u of l) {
+  for (const e of l) {
     const l = []
-    for (const e of n) {
-      const [n, t, r] = u
-      for (let u = n; u <= t; u += r) {
-        const n = [...e, u]
+    for (const u of n) {
+      const [n, t, r] = e
+      for (let e = n; e <= t; e += r) {
+        const n = [...u, e]
         l.push(n)
       }
     }
@@ -19,10 +19,10 @@ function u(l) {
   }
   return n
 }
-function e(l, ...n) {
-  const u = new Set(l)
-  for (const l of n) for (const n of l) u.delete(n)
-  return u
+function u(l, ...n) {
+  const e = new Set(l)
+  for (const l of n) for (const n of l) e.delete(n)
+  return e
 }
 new Map()
 class t {
@@ -40,14 +40,14 @@ class t {
   }
   set(l, n) {
     l = this._convertKeyToArray(l)
-    let u = this._cache
+    let e = this._cache
     for (const n of l.slice(0, l.length - 1))
-      if (u instanceof Map && u.has(n)) u = u.get(n)
+      if (e instanceof Map && e.has(n)) e = e.get(n)
       else {
         const l = new Map()
-        u.set(n, l), (u = l)
+        e.set(n, l), (e = l)
       }
-    u.set(
+    e.set(
       (function (l) {
         const n = l.length
         return n >= 1 ? l[n - 1] : null
@@ -64,12 +64,12 @@ class t {
   }
   _retrieveValue(l, n) {
     l = this._convertKeyToArray(l)
-    let u = this._cache
-    for (const e of l) {
-      if (!(u instanceof Map && u.has(e))) return n()
-      u = u.get(e)
+    let e = this._cache
+    for (const u of l) {
+      if (!(e instanceof Map && e.has(u))) return n()
+      e = e.get(u)
     }
-    return u
+    return e
   }
 }
 class r {
@@ -88,8 +88,8 @@ class r {
     }
   }
   set(l, n) {
-    const u = this._convertKeyToArray(l)
-    this._cache.set(u, n)
+    const e = this._convertKeyToArray(l)
+    this._cache.set(e, n)
   }
   clear() {
     this._cache.clear()
@@ -104,7 +104,7 @@ class r {
 }
 function o(...l) {
   const n = new Set()
-  for (const u of l) for (const l of u) n.add(l)
+  for (const e of l) for (const l of e) n.add(l)
   return n
 }
 new r(), new r()
@@ -121,35 +121,35 @@ const c = [
 ]
 function s(l) {
   const n = l.querySelectorAll('input'),
-    u = new Array()
+    e = new Array()
   for (let l = 0; l < 9; l++) {
-    const e = new Array()
-    u.push(e)
-    for (let u = 0; u < 9; u++) {
-      const t = n[9 * l + u].value,
+    const u = new Array()
+    e.push(u)
+    for (let e = 0; e < 9; e++) {
+      const t = n[9 * l + e].value,
         r = t ? Number(t) : 0
-      e.push(r)
+      u.push(r)
     }
   }
-  return u
+  return e
 }
 function a(l) {
   const n = document.createElement('table'),
-    u = document.createElement('tbody')
-  n.appendChild(u)
+    e = document.createElement('tbody')
+  n.appendChild(e)
   for (let n = 0; n < 3; n++) {
-    const e = document.createElement('tr')
-    for (let u = 0; u < 3; u++) {
+    const u = document.createElement('tr')
+    for (let e = 0; e < 3; e++) {
       const t = document.createElement('td'),
-        r = 3 * n + u + 1
-      ;(t.innerHTML = l && l.includes(r) ? r : '&nbsp;'), e.appendChild(t)
+        r = 3 * n + e + 1
+      ;(t.innerHTML = l && l.includes(r) ? r : '&nbsp;'), u.appendChild(t)
     }
-    u.appendChild(e)
+    e.appendChild(u)
   }
   return n
 }
 function i(n) {
-  const u = [
+  const e = [
     [null, null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null, null],
@@ -163,21 +163,21 @@ function i(n) {
   for (let t = 0; t < n.length; t++)
     for (let r = 0; r < n[t].length; r++)
       n[t][r] ||
-        (u[t][r] = [
-          ...e(
+        (e[t][r] = [
+          ...u(
             new Set([1, 2, 3, 4, 5, 6, 7, 8, 9]),
             o(o(new Set(m(n, t)), new Set(p(n, r))), new Set(l(...y(n, t, r))))
           ),
         ])
-  for (let l = 0; l < u.length; l++)
-    for (let n = 0; n < u[l].length; n++)
-      for (let t = n + 1; t < u[l].length; t++) {
-        const r = u[l][n],
-          c = u[l][t]
+  for (let l = 0; l < e.length; l++)
+    for (let n = 0; n < e[l].length; n++)
+      for (let t = n + 1; t < e[l].length; t++) {
+        const r = e[l][n],
+          c = e[l][t]
         if (r && c && 2 === r.length && 2 === c.length && 2 === o(r, c).size) {
-          for (let n = 0; n < u[l].length; n++) {
-            const t = u[l][n]
-            t && t !== r && t !== c && (u[l][n] = Array.from(e(t, new Set(r))))
+          for (let n = 0; n < e[l].length; n++) {
+            const t = e[l][n]
+            t && t !== r && t !== c && (e[l][n] = Array.from(u(t, new Set(r))))
           }
           if (v({ rowIndex: l, columnIndex: n }, { rowIndex: l, columnIndex: t })) {
             const o = g(l),
@@ -186,55 +186,55 @@ function i(n) {
               for (let a = 0; a < 3; a++) {
                 const i = o + s,
                   d = c + a
-                u[i][d] &&
+                e[i][d] &&
                   !((i === l && d === n) || (i === l && d === t)) &&
-                  (u[i][d] = Array.from(e(u[i][d], new Set(r))))
+                  (e[i][d] = Array.from(u(e[i][d], new Set(r))))
               }
           }
         }
       }
-  for (let l = 0; l < u[0].length; l++)
-    for (let n = 0; n < u.length; n++)
-      for (let t = n + 1; t < u.length; t++) {
-        const r = u[n][l],
-          c = u[t][l]
+  for (let l = 0; l < e[0].length; l++)
+    for (let n = 0; n < e.length; n++)
+      for (let t = n + 1; t < e.length; t++) {
+        const r = e[n][l],
+          c = e[t][l]
         if (r && c && 2 === r.length && 2 === c.length && 2 === o(r, c).size)
-          for (let n = 0; n < u.length; n++) {
-            const t = u[n][l]
-            t && t !== r && t !== c && (u[n][l] = Array.from(e(t, new Set(r))))
+          for (let n = 0; n < e.length; n++) {
+            const t = e[n][l]
+            t && t !== r && t !== c && (e[n][l] = Array.from(u(t, new Set(r))))
           }
       }
-  return u
+  return e
 }
-function d(l, n, u) {
-  const e = [...m(l, n)]
-  return e.splice(u, 1), e
+function d(l, n, e) {
+  const u = [...m(l, n)]
+  return u.splice(e, 1), u
 }
-function f(l, n, u) {
-  const e = [...p(l, u)]
-  return e.splice(n, 1), e
+function f(l, n, e) {
+  const u = [...p(l, e)]
+  return u.splice(n, 1), u
 }
-function h(l, n, u) {
-  const e = y(l, n, u)
-  return (e[n % 3] = [...e[n % 3]]), e[n % 3].splice(u % 3, 1, null), e
+function h(l, n, e) {
+  const u = y(l, n, e)
+  return (u[n % 3] = [...u[n % 3]]), u[n % 3].splice(e % 3, 1, null), u
 }
 function m(l, n) {
   return l[n]
 }
 function p(l, n) {
-  const u = l.length,
-    e = new Array(u)
-  for (let t = 0; t < u; t++) e[t] = l[t][n]
-  return e
+  const e = l.length,
+    u = new Array(e)
+  for (let t = 0; t < e; t++) u[t] = l[t][n]
+  return u
 }
-function y(l, n, u) {
-  ;(n = g(n)), (u = w(u))
-  const e = new Array(3)
+function y(l, n, e) {
+  ;(n = g(n)), (e = w(e))
+  const u = new Array(3)
   for (let t = 0; t < 3; t++) {
-    e[t] = new Array(3)
-    for (let r = 0; r < 3; r++) e[t][r] = l[n + t][u + r]
+    u[t] = new Array(3)
+    for (let r = 0; r < 3; r++) u[t][r] = l[n + t][e + r]
   }
-  return e
+  return u
 }
 function g(l) {
   return 3 * Math.floor(l / 3)
@@ -243,77 +243,77 @@ function w(l) {
   return 3 * Math.floor(l / 3)
 }
 function v(l, n) {
-  const { rowIndex: u, columnIndex: e } = l,
+  const { rowIndex: e, columnIndex: u } = l,
     { rowIndex: t, columnIndex: r } = n
-  return g(u) === g(t) && w(e) === w(r)
+  return g(e) === g(t) && w(u) === w(r)
 }
 function E(l) {
-  if (_(l)) return l
+  if (b(l)) return l
   const n = i(l)
   if (
     (function (l, n) {
-      return u([
+      return e([
         [0, 8, 1],
         [0, 8, 1],
-      ]).every(([u, e]) => !(!l[u][e] && !n[u][e]))
+      ]).every(([e, u]) => !(!l[e][u] && !n[e][u]))
     })(l, n)
   ) {
-    let u
-    u = l[0][0] ? b(l, 0, 0) : { row: 0, column: 0 }
-    const e = (function (l) {
+    let e
+    e = l[0][0] ? A(l, 0, 0) : { row: 0, column: 0 }
+    const u = (function (l) {
       return l.map(l => Array.from(l))
     })(l)
-    return C(l, e, n, u.row, u.column)
+    return S(l, u, n, e.row, e.column)
   }
   return null
 }
-function C(l, n, u, e, t) {
+function S(l, n, e, u, t) {
   const r = (function (...l) {
     const n = new Set(l[0])
-    for (const u of l.slice(1)) for (const l of u) n.delete(l)
+    for (const e of l.slice(1)) for (const l of e) n.delete(l)
     return n
-  })(new Set(u[e][t]), m(n, e), p(n, t), y(n, e, t))
+  })(new Set(e[u][t]), m(n, u), p(n, t), y(n, u, t))
   if (r.size >= 1) {
-    const o = b(l, e, t)
+    const o = A(l, u, t)
     for (const c of r)
-      if (((n[e][t] = c), o)) {
-        const e = C(l, n, u, o.row, o.column)
-        if (e) return e
-      } else if (_(n)) return n
+      if (((n[u][t] = c), o)) {
+        const u = S(l, n, e, o.row, o.column)
+        if (u) return u
+      } else if (b(n)) return n
   }
-  return (n[e][t] = null), null
+  return (n[u][t] = null), null
 }
-function b(l, n, u) {
-  const e =
+function A(l, n, e) {
+  const u =
     (function (l, n) {
       return 9 * l + n
-    })(n, u) + 1
-  for (let n = e; n < 81; n++) {
-    const { row: u, column: e } = S(n)
-    if (!l[u][e]) return { row: u, column: e }
+    })(n, e) + 1
+  for (let n = u; n < 81; n++) {
+    const { row: e, column: u } = C(n)
+    if (!l[e][u]) return { row: e, column: u }
   }
   return null
 }
-function S(l) {
+function C(l) {
   return { row: Math.floor(l / 9), column: l % 9 }
 }
-function _(l) {
+function b(l) {
   return (
     (function (l) {
       return [...n(0, 8)].map(n => m(l, n))
-    })(l).every(A) &&
+    })(l).every(_) &&
     (function (l) {
       return [...n(0, 8)].map(n => p(l, n))
     })(l).every(L) &&
     (function (l) {
-      return u([
+      return e([
         [0, 8, 3],
         [0, 8, 3],
-      ]).map(([n, u]) => y(l, n, u).flat())
+      ]).map(([n, e]) => y(l, n, e).flat())
     })(l).every(T)
   )
 }
-function A(l) {
+function _(l) {
   return M(l)
 }
 function L(l) {
@@ -326,9 +326,9 @@ const x = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9])
 function M(l) {
   return 9 === new Set(l).size && l.every(l => x.has(l))
 }
-let I
-const k = localStorage.getItem('sudoku')
-I = k ? JSON.parse(k) : [...c]
+let k
+const I = localStorage.getItem('sudoku')
+k = I ? JSON.parse(I) : [...c]
 let K = [
     [null, null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null, null],
@@ -355,39 +355,40 @@ function O() {
   document.body.innerHTML = ''
   const n = document.createElement('div')
   n.classList.add('row')
-  const u = document.createElement('div')
-  u.classList.add('column')
   const e = document.createElement('div')
   e.classList.add('column')
+  const u = document.createElement('div')
+  u.classList.add('column')
   const t = document.createElement('div')
-  t.classList.add('column'), n.append(u, e, t), document.body.appendChild(n)
+  t.classList.add('column'), n.append(e, u, t), document.body.appendChild(n)
   const r = (function (l) {
-    const n = document.createElement('table')
-    n.classList.add('sudoku'), n.classList.add('sudoku-input')
-    const u = document.createElement('tbody')
-    n.appendChild(u)
-    for (let n = 0; n < l.length; n++) {
-      const e = document.createElement('tr')
-      for (let u = 0; u < l[n].length; u++) {
-        const t = document.createElement('td'),
-          r = document.createElement('input')
-        r.maxLength = 1
-        const o = l[n][u]
-        0 !== o && (r.value = o), t.appendChild(r), e.appendChild(t)
+      const n = document.createElement('table')
+      n.classList.add('sudoku'), n.classList.add('sudoku-input')
+      const e = document.createElement('tbody')
+      n.appendChild(e)
+      for (let n = 0; n < l.length; n++) {
+        const u = document.createElement('tr')
+        for (let e = 0; e < l[n].length; e++) {
+          const t = document.createElement('td'),
+            r = document.createElement('input')
+          ;(r.type = 'text'), (r.inputMode = 'numeric'), (r.pattern = '[1-9]'), (r.maxLength = 1)
+          const o = l[n][e]
+          0 !== o && (r.value = o), t.appendChild(r), u.appendChild(t)
+        }
+        e.appendChild(u)
       }
-      u.appendChild(e)
-    }
-    return n
-  })(I)
-  ;(r.querySelector('input').autofocus = !0), u.appendChild(r), u.appendChild(document.createElement('br'))
-  const o = document.createElement('button')
-  ;(o.innerText = 'Next'),
-    (o.style.marginRight = '0.5rem'),
-    o.addEventListener('click', () => {
-      ;(I = s(r)),
-        (K = i(I)),
+      return n
+    })(k),
+    o = r.querySelector('input')
+  ;(o.autofocus = !0), e.appendChild(r), o.select(), e.appendChild(document.createElement('br'))
+  const m = document.createElement('button')
+  ;(m.innerText = 'Next'),
+    (m.style.marginRight = '0.5rem'),
+    m.addEventListener('click', () => {
+      ;(k = s(r)),
+        (K = i(k)),
         (N = (function (n) {
-          const u = [
+          const e = [
             [null, null, null, null, null, null, null, null, null],
             [null, null, null, null, null, null, null, null, null],
             [null, null, null, null, null, null, null, null, null],
@@ -398,58 +399,58 @@ function O() {
             [null, null, null, null, null, null, null, null, null],
             [null, null, null, null, null, null, null, null, null],
           ]
-          for (let e = 0; e < n.length; e++)
-            for (let t = 0; t < n[e].length; t++)
-              if (n[e][t]) {
-                const r = n[e][t]
+          for (let u = 0; u < n.length; u++)
+            for (let t = 0; t < n[u].length; t++)
+              if (n[u][t]) {
+                const r = n[u][t]
                 let o
                 if (1 === r.length) o = r[0]
                 else {
-                  const u = new Set(l(...d(n, e, t))),
-                    c = new Set(l(...f(n, e, t))),
-                    s = new Set(l(...l(...h(n, e, t))))
-                  o = r.find(l => !u.has(l) || !c.has(l) || !s.has(l))
+                  const e = new Set(l(...d(n, u, t))),
+                    c = new Set(l(...f(n, u, t))),
+                    s = new Set(l(...l(...h(n, u, t))))
+                  o = r.find(l => !e.has(l) || !c.has(l) || !s.has(l))
                 }
-                o && (u[e][t] = o)
+                o && (e[u][t] = o)
               }
-          return u
+          return e
         })(K)),
-        (I = (function (l, n) {
+        (k = (function (l, n) {
           l = [...l]
-          for (let u = 0; u < l.length; u++) for (let e = 0; e < l[u].length; e++) n[u][e] && (l[u][e] = n[u][e])
+          for (let e = 0; e < l.length; e++) for (let u = 0; u < l[e].length; u++) n[e][u] && (l[e][u] = n[e][u])
           return l
-        })(I, N)),
+        })(k, N)),
         O()
     }),
-    u.appendChild(o)
-  const m = document.createElement('button')
-  ;(m.innerText = 'Brute force'),
-    (m.style.marginRight = '0.5rem'),
-    m.addEventListener('click', () => {
-      const l = E(I)
-      l && ((N = l.map((l, n) => l.map((l, u) => (I[n][u] ? null : l)))), (I = l)), O()
-    }),
-    u.appendChild(m)
+    e.appendChild(m)
   const p = document.createElement('button')
-  ;(p.innerText = 'Reset'),
+  ;(p.innerText = 'Brute force'),
+    (p.style.marginRight = '0.5rem'),
     p.addEventListener('click', () => {
-      ;(I = [...c]), O()
+      const l = E(k)
+      l && ((N = l.map((l, n) => l.map((l, e) => (k[n][e] ? null : l)))), (k = l)), O()
     }),
-    u.appendChild(p),
-    e.appendChild(
+    e.appendChild(p)
+  const y = document.createElement('button')
+  ;(y.innerText = 'Reset'),
+    y.addEventListener('click', () => {
+      ;(k = [...c]), O()
+    }),
+    e.appendChild(y),
+    u.appendChild(
       (function (l) {
         const n = document.createElement('table')
         n.classList.add('sudoku')
-        const u = document.createElement('tbody')
-        n.appendChild(u)
+        const e = document.createElement('tbody')
+        n.appendChild(e)
         for (let n = 0; n < l.length; n++) {
-          const e = document.createElement('tr')
-          for (let u = 0; u < l[n].length; u++) {
+          const u = document.createElement('tr')
+          for (let e = 0; e < l[n].length; e++) {
             const t = document.createElement('td'),
-              r = l[n][u]
-            0 === r ? (t.innerHTML = '&nbsp;') : (t.innerText = r), e.appendChild(t)
+              r = l[n][e]
+            0 === r ? (t.innerHTML = '&nbsp;') : (t.innerText = r), u.appendChild(t)
           }
-          u.appendChild(e)
+          e.appendChild(u)
         }
         return n
       })(N)
@@ -458,21 +459,42 @@ function O() {
       (function (l) {
         const n = document.createElement('table')
         n.classList.add('possible-numbers')
-        const u = document.createElement('tbody')
-        n.appendChild(u)
+        const e = document.createElement('tbody')
+        n.appendChild(e)
         for (let n = 0; n < l.length; n++) {
-          const e = document.createElement('tr')
-          for (let u = 0; u < l[n].length; u++) {
+          const u = document.createElement('tr')
+          for (let e = 0; e < l[n].length; e++) {
             const t = document.createElement('td')
-            t.appendChild(a(l[n][u])), e.appendChild(t)
+            t.appendChild(a(l[n][e])), u.appendChild(t)
           }
-          u.appendChild(e)
+          e.appendChild(u)
         }
         return n
       })(K)
-    ),
+    )
+  const g = new Set(['1', '2', '3', '4', '5', '6', '7', '8', '9'])
+  document.addEventListener('keydown', function (l) {
+    const { target: n } = l
+    'INPUT' !== n.tagName || g.has(l.key) || l.preventDefault()
+  }),
     document.addEventListener('keyup', function (l) {
-      'INPUT' === l.target.tagName && localStorage.setItem('sudoku', JSON.stringify(s(r)))
+      const { target: n } = l
+      if ('INPUT' === n.tagName && g.has(l.key)) {
+        const l = parseInt(n.value, 10)
+        if (l >= 1 && l <= 9) {
+          const l = (function (l) {
+            const n = Array.from(r.querySelectorAll('input')),
+              e = n.indexOf(l)
+            if (-1 !== e) {
+              const l = e + 1
+              if (l < n.length) return n[l]
+            }
+            return null
+          })(n)
+          l && (l.focus(), l.select())
+        }
+        localStorage.setItem('sudoku', JSON.stringify(s(r)))
+      }
     })
 }
 document.addEventListener('DOMContentLoaded', () => {
