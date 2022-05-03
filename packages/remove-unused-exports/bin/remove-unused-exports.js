bd626235c24691b7034987872da02d83469c9573
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-05-03 21:14:26 +0200

    Some changed files

--- packages/remove-unused-exports/bin/remove-unused-exports.js
+++ packages/remove-unused-exports/bin/remove-unused-exports.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { removeExports } from '../index.js';
+import { removeExports } from "../index.js";
 await removeExports();
 //# sourceMappingURL=remove-unused-exports.js.map
