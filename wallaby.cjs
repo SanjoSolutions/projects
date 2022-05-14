8fa1c98cfef02d7d5a5a7d7f4e417d1845a49783
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-05-14 14:22:25 +0200

    Exclude "packages/compose/example/**/*"
    to potentially fix a recursive directory reference error outputted by Wallaby.

--- wallaby.cjs
+++ wallaby.cjs
@@ -6,12 +6,14 @@ module.exports = () => ({
     '!packages/*/src/**/*.spec.ts',
     'rgbToHsl.js',
     'sudoku_solver/*.js',
-    '!sudoku_solver/*.spec.js'
+    '!sudoku_solver/*.spec.js',
+    '!packages/compose/example/**/*'
   ],
 
   tests: [
     'packages/*/src/**/*.spec.ts',
     'rgbToHsl.spec.js',
-    'sudoku_solver/*.spec.js'
+    'sudoku_solver/*.spec.js',
+    '!packages/compose/example/**/*'
   ],
 })
