5607109f6a0beddedf015c5db5227f6340e03d71
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-07-26 16:53:09 +0200

    din-5008-schreiben: more automation for updating the credentials

--- din-5008-schreiben/functions/src/authorize.ts
+++ din-5008-schreiben/functions/src/authorize.ts
@@ -1,9 +1,20 @@
-import { authenticate } from './authenticate.js'
+import { readJSON } from "@sanjo/read-json";
+import admin from "firebase-admin";
+import { authenticate } from "./authenticate.js";
+import { saveCredentials } from "./saveCredentials.js";
 
 async function main() {
-  const auth = await authenticate()
-  const credentials = auth.credentials
-  console.log('credentials', credentials)
+  const auth = await authenticate();
+  const credentials = auth.credentials;
+
+  const serviceAccount = await readJSON(
+    "din-5008-schreiben-firebase-adminsdk-bctqd-ebe4a9ee76.json"
+  );
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://din-5008-schreiben.firebaseio.com",
+  });
+  await saveCredentials(credentials);
 }
 
-main().catch(console.error)
+main().catch(console.error);
