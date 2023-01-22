0238adae652b917c2f9cbb5d81865eec65322af3
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-01-22 19:16:43 +0100

    Updates header

--- din-5008-schreiben/functions/src/processMessage.ts
+++ din-5008-schreiben/functions/src/processMessage.ts
@@ -1,35 +1,39 @@
-import { createGmail } from './createGmail.js'
-import { emailAddress } from './emailAddress.js'
-import type { Header } from './Header.js'
-import { respondToMessage } from './respondToMessage.js'
-import { updateTokenIfUpdated } from './updateTokenIfUpdates.js'
+import { createGmail } from "./createGmail.js";
+import { emailAddress } from "./emailAddress.js";
+import type { Header } from "./Header.js";
+import { respondToMessage } from "./respondToMessage.js";
+import { updateTokenIfUpdated } from "./updateTokenIfUpdates.js";
 
 export async function processMessage(message: any) {
-  const { auth, gmail } = await createGmail()
+  const { auth, gmail } = await createGmail();
 
   const response = await gmail.users.messages.list({
     userId: emailAddress,
-    labelIds: ['UNREAD', 'INBOX'],
-    q: 'from:drive-shares-dm-noreply@google.com is:unread',
-  })
+    labelIds: ["UNREAD", "INBOX"],
+    q: "from:drive-shares-dm-noreply@google.com is:unread",
+  });
 
   const messages = await Promise.all(
     (response.data.messages ?? []).map(async ({ id }) => {
       const response = await gmail.users.messages.get({
         userId: emailAddress,
         id: id!,
-      })
-      return response.data
+      });
+      return response.data;
     })
-  )
+  );
 
   const sharingRequestMessagesForDocument = messages.filter((message: any) =>
     message.payload.headers.some(
-      (header: Header) => header.name === 'Subject' && header.value === 'Share request for ‘DIN-5008 Template’'
+      (header: Header) =>
+        header.name === "Subject" &&
+        header.value === 'Share request for "DIN-5008 Template"'
     )
-  )
+  );
 
-  await Promise.allSettled(sharingRequestMessagesForDocument.map(respondToMessage.bind(null, gmail)))
+  await Promise.allSettled(
+    sharingRequestMessagesForDocument.map(respondToMessage.bind(null, gmail))
+  );
 
-  await updateTokenIfUpdated(auth)
+  await updateTokenIfUpdated(auth);
 }
