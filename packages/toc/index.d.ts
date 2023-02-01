d5b85a80f4a5e3ec24ae1fd23e936d5eabb77ee8
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-02-01 07:00:28 +0100

    Adds function "retrieveVersion"

--- packages/toc/index.d.ts
+++ packages/toc/index.d.ts
@@ -1,3 +1,5 @@
 export declare const dependenciesRegExp: RegExp;
 export declare function retrieveDependencies(tocFilePath: string): Promise<string[]>;
+export declare const versionRegExp: RegExp;
+export declare function retrieveVersion(tocFilePath: string): Promise<string | null>;
 //# sourceMappingURL=index.d.ts.map
--- packages/toc/src/index.ts
+++ packages/toc/src/index.ts
@@ -8,3 +8,13 @@ export async function retrieveDependencies(tocFilePath: string) {
   const dependencies = match ? match[2].split(", ") : [];
   return dependencies;
 }
+
+export const versionRegExp = /## Version: (\d+\.\d+\.\d+)/;
+
+export async function retrieveVersion(
+  tocFilePath: string
+): Promise<string | null> {
+  const content = await readFile(tocFilePath);
+  const match = versionRegExp.exec(content);
+  return match ? match[1] : null;
+}
