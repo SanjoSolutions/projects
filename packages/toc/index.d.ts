3ca05c0dba49fd7cb80446a8bd4924a351c9ea4a
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-04-09 13:22:24 +0200

    Libs dependency embedding (some work for it)

--- packages/toc/index.d.ts
+++ packages/toc/index.d.ts
@@ -1,4 +1,8 @@
 export declare function retrieveDependencies(tocFilePath: string): Promise<string[]>;
 export declare const versionRegExp: RegExp;
 export declare function retrieveVersion(tocFilePath: string): Promise<string | null>;
+export declare function extractListedFiles(tocFilePath: string): Promise<string[]>;
+export declare function retrieveAddOnTOCFilePath(addOnPath: string): string;
+export declare function retrieveAddOnName(addOnPath: string): string;
+export declare function prependFilesToLoad(addOnTocFilePath: string, filesToLoad: string[]): Promise<void>;
 //# sourceMappingURL=index.d.ts.map
--- packages/toc/src/index.ts
+++ packages/toc/src/index.ts
@@ -1,4 +1,6 @@
-import { readFile } from "@sanjo/read-file";
+import { readFile } from '@sanjo/read-file';
+import { writeFile } from '@sanjo/write-file';
+import * as path from 'node:path'
 
 export async function retrieveDependencies(tocFilePath: string) {
   const content = await readFile(tocFilePath);
@@ -13,10 +15,86 @@ export async function retrieveDependencies(tocFilePath: string) {
 
 export const versionRegExp = /## Version: (\d+\.\d+\.\d+)/;
 
-export async function retrieveVersion(
-  tocFilePath: string
-): Promise<string | null> {
+export async function retrieveVersion(tocFilePath: string): Promise<string | null> {
   const content = await readFile(tocFilePath);
   const match = versionRegExp.exec(content);
   return match ? match[1] : null;
+}
+
+export async function extractListedFiles(tocFilePath: string): Promise<string[]> {
+  const content = await readFile(tocFilePath);
+  const lines = content.split(/(?:\n|\r\n|\r)/);
+  const loadFileLines = lines.filter(isLoadFileLine);
+  const loadedFiles = loadFileLines.map(line => line.trim());
+  return loadedFiles;
+}
+
+function isLoadFileLine(line: string): boolean {
+  const trimmedLine = line.trim();
+  return trimmedLine.length >= 1 && !isCommentLine(trimmedLine);
+}
+
+const COMMENT_LINE_REGEXP = /^##/;
+
+function isCommentLine(line: string): boolean {
+  return COMMENT_LINE_REGEXP.test(line);
+}
+
+enum GameVersion {
+  'default' = 'default', 'vanilla' = 'vanilla', 'wrath' = 'wrath'
+}
+type Dependency = string
+type ResolvedDependenciesForGameVersions = Map<GameVersion, Dependency[]>
+
+async function resolveDependencies(addOn: string): Promise<ResolvedDependenciesForGameVersions> {
+  const dependencies = new Map();
+  const loadOrder = [];
+  const alreadyLoadedAddOns = new Set();
+  const resolvedAddOns = new Set();
+
+  function addAddOnToLoadOrder(addOn: string) {
+    loadOrder.push(addOn);
+    alreadyLoadedAddOns.add(addOn);
+  }
+
+  resolvedAddOns.add(addOn);
+  const content = await readTOCFile(addOn);
+  const dependenciesRegExp = /^## (?:Dep\w*|RequireDeps): *(.+) *$/m;
+  const match = dependenciesRegExp.exec(content);
+  const addOnDependencies = match ? match[1].split(', ') : [];
+  dependencies.set(addOn, addOnDependencies);
+  for (const addOnDependencyName of addOnDependencies) {
+    if (!resolvedAddOns.has(addOnDependencyName)) {
+      await resolveDependencies(addOnDependencyName);
+    }
+  }
+  const addOnsStillToLoad = addOnDependencies.filter(addOn => !alreadyLoadedAddOns.has(addOn));
+  addOnsStillToLoad.forEach(addAddOnToLoadOrder);
+  if (!alreadyLoadedAddOns.has(addOn)) {
+    addAddOnToLoadOrder(addOn);
+  }
+}
+
+async function readTOCFile(tocFilePath: string): Promise<string> {
+  return await readFile(tocFilePath);
+}
+
+export function retrieveAddOnTOCFilePath(addOnPath: string): string {
+  const addOnName = retrieveAddOnName(addOnPath)
+  const addOnTocFilePath = path.join(addOnPath, `${ addOnName }.toc`)
+  return addOnTocFilePath
+}
+
+export function retrieveAddOnName(addOnPath: string): string {
+  return path.basename(addOnPath)
+}
+
+export async function prependFilesToLoad(addOnTocFilePath: string, filesToLoad: string[]): Promise<void> {
+  let content = await readFile(addOnTocFilePath);
+  const lines = content.split(/(?:\n|\r\n|\r)/);
+  let firstLoadFileLineIndex = lines.findIndex(isLoadFileLine)
+  const indexToInsert = firstLoadFileLineIndex !== -1 ? firstLoadFileLineIndex : lines.length
+  lines.splice(indexToInsert, 0, ...filesToLoad)
+  content = lines.join('\n')
+  await writeFile(addOnTocFilePath, content)
 }
