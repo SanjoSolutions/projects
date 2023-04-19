f30db5aa248ceda9d4a2849fd1851637532f44ee
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-04-19 13:03:07 +0200

    Adds capability to remove event handlers

--- makeMovable.js
+++ makeMovable.js
@@ -1,55 +1,80 @@
-import { identity } from './packages/identity/src/identity.js'
+import { identity } from "./packages/identity/src/identity.js";
 
 export function makeMovable(
   element,
-  { elementWithWhichTheElementCanBeMovedWith, onPointerDown, onPointerUp, isMoving } = {}
+  {
+    elementWithWhichTheElementCanBeMovedWith,
+    onPointerDown,
+    onPointerUp,
+    isMoving,
+  } = {}
 ) {
   if (!elementWithWhichTheElementCanBeMovedWith) {
-    elementWithWhichTheElementCanBeMovedWith = element
+    elementWithWhichTheElementCanBeMovedWith = element;
   }
   if (!onPointerDown) {
-    onPointerDown = identity
+    onPointerDown = identity;
   }
   if (!onPointerUp) {
-    onPointerUp = identity
+    onPointerUp = identity;
   }
   if (!isMoving) {
-    isMoving = () => true
+    isMoving = () => true;
   }
-  const { x, y } = element.getBoundingClientRect()
-  element.style.position = 'absolute'
-  element.style.left = x + 'px'
-  element.style.top = y + 'px'
+  const { x, y } = element.getBoundingClientRect();
+  element.style.position = "absolute";
+  element.style.left = x + "px";
+  element.style.top = y + "px";
 
-  let isMousePressed = false
-  let pointerClickOffsetToWindow = null
-  elementWithWhichTheElementCanBeMovedWith.addEventListener('pointerdown', function (event) {
-    onPointerDown(event)
-    isMousePressed = true
+  let isMousePressed = false;
+  let pointerClickOffsetToWindow = null;
+
+  function onPointerDownHandler(event) {
+    onPointerDown(event);
+    isMousePressed = true;
     pointerClickOffsetToWindow = {
       x: event.clientX - element.offsetLeft,
       y: event.clientY - element.offsetTop,
-    }
-    event.preventDefault()
-  })
+    };
+    event.preventDefault();
+  }
 
-  window.addEventListener('pointermove', function (event) {
+  elementWithWhichTheElementCanBeMovedWith.addEventListener(
+    "pointerdown",
+    onPointerDownHandler
+  );
+
+  function onPointerMove(event) {
     if (isMousePressed && isMoving(event)) {
-      const x = event.clientX - pointerClickOffsetToWindow.x
-      const y = event.clientY - pointerClickOffsetToWindow.y
+      const x = event.clientX - pointerClickOffsetToWindow.x;
+      const y = event.clientY - pointerClickOffsetToWindow.y;
 
-      element.style.position = 'absolute'
-      element.style.left = x + 'px'
-      element.style.top = y + 'px'
+      element.style.position = "absolute";
+      element.style.left = x + "px";
+      element.style.top = y + "px";
     }
-  })
-  window.addEventListener('pointerup', function (event) {
+  }
+
+  window.addEventListener("pointermove", onPointerMove);
+
+  function onPointerUpHandler(event) {
     if (isMousePressed) {
-      onPointerUp(event)
-      isMousePressed = false
-      pointerClickOffsetToWindow = null
+      onPointerUp(event);
+      isMousePressed = false;
+      pointerClickOffsetToWindow = null;
     }
-  })
+  }
 
-  return element
+  window.addEventListener("pointerup", onPointerUpHandler);
+
+  function removeEventListeners() {
+    elementWithWhichTheElementCanBeMovedWith.removeEventListener(
+      "pointerdown",
+      onPointerDownHandler
+    );
+    window.removeEventListener("pointermove", onPointerMove);
+    window.removeEventListener("pointerup", onPointerUpHandler);
+  }
+
+  return removeEventListeners;
 }
